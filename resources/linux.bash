75c234baacc90123ebf4bfe80471007c824970f3
HamimSeam <hseam30@stuy.edu>
2023-06-01 09:42:45 -0400

    added txt files of volatility plugin outputs

---
+++ resources/linux.bash
@@ -0,0 +1,15 @@
+Volatility 3 Framework 2.4.2
+
+PID	Process	CommandTime	Command
+
+43824	bash	2023-05-31 23:58:21.000000 	history
+43824	bash	2023-05-31 23:58:24.000000 	clear
+43824	bash	2023-05-31 23:58:24.000000 	ls
+43824	bash	2023-05-31 23:59:40.000000 	GTK_MODULES=gail:atk-bridge
+43824	bash	2023-05-31 23:59:40.000000 	curl cheat.sh/curl
+43824	bash	2023-05-31 23:59:40.000000 	��UH��H�H��t�z���H��]�q������U1����������]�����f.�
+43824	bash	2023-06-01 00:02:00.000000 	wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+43824	bash	2023-06-01 00:02:34.000000 	fortune -l | cowsay | lolcat
+43824	bash	2023-06-01 00:02:55.000000 	sudo ./avml memory.dmp
+44245	bash	2023-05-31 23:58:57.000000 	ssh hseam30@149.89.161.103
+44429	bash	2023-06-01 00:03:33.000000 	ls -l memory.dmp 
